35285ed3fe45a3b65e58ad4d45ec4de9642df65c
Wiktor Kania <roller47@wp.pl>
2022-05-08 15:02:27 +0200

    compile to a single file

--- out/error-handler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorHandler = void 0;
-class ErrorHandler {
-    static throwError(cause) {
-        throw new Error(cause);
-    }
-}
-exports.ErrorHandler = ErrorHandler;
